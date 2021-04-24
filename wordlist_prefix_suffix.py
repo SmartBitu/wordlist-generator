88ce18a2bf394a771580aa2dcda061f8334faf7b
Shreyash Rajam <67874000+SmartBitu@users.noreply.github.com>
2021-04-24 14:02:45 +0530

    Add files via upload

---
+++ wordlist_prefix_suffix.py
@@ -0,0 +1,23 @@
+pre = input("Enter prefix for every word in list  :  ")
+suf = input("Enter suffix for every word in list  :  ")
+lst = []
+
+n = 99999999999
+# iterating till the range
+for i in range(0, n):
+    ele = input("Enter word list 1 by 1 or copy all and paste   (Or you want to submit list then type Y) : ")
+    if ele == "Y":
+        break
+    else:
+        result = pre + ele + suf
+        lst.append(result)  # adding the element
+print(lst)
+# save in file or not
+file=input("Enter file name to save wordlist in .txt format (OR Don't want to save then type N)  :  ")
+if file != "N":
+    with open(file+".txt", 'w') as output:
+        for row in lst:
+            output.write(str(row) + '\n')
+    print("Go to "+file+".txt and see the wordlist generated........")
+print("Done !!!")
+
