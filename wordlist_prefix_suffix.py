1226412b89aa491723b67e9406721b9318ea3214
Shreyash Rajam <67874000+SmartBitu@users.noreply.github.com>
2021-04-24 14:04:21 +0530

    Update wordlist_prefix_suffix.py

--- wordlist_prefix_suffix.py
+++ wordlist_prefix_suffix.py
@@ -1,9 +1,10 @@
+# getting input for prefix and suffix for list
 pre = input("Enter prefix for every word in list  :  ")
 suf = input("Enter suffix for every word in list  :  ")
 lst = []
 
 n = 99999999999
-# iterating till the range
+# iterating till the range and getting list of words.
 for i in range(0, n):
     ele = input("Enter word list 1 by 1 or copy all and paste   (Or you want to submit list then type Y) : ")
     if ele == "Y":
@@ -12,6 +13,7 @@ for i in range(0, n):
         result = pre + ele + suf
         lst.append(result)  # adding the element
 print(lst)
+
 # save in file or not
 file=input("Enter file name to save wordlist in .txt format (OR Don't want to save then type N)  :  ")
 if file != "N":
